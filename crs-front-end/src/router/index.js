beabd49bcfa9c8200b2fffc6b291f5b0fd9fc111
曾加俊 <1136329229@qq.com>
2023-05-05 12:04:17 +0800

    m

--- crs-front-end/src/router/index.js
+++ crs-front-end/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import LoginView from "@/views/LoginView";
 import HomeView from "@/views/HomeView";
+import {MessageBox} from "element-ui";
 Vue.use(VueRouter)
 
 const routes = [
@@ -14,6 +15,24 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  let account = localStorage.getItem('account');
+  if (to.path == '/') {
+    next()
+    return
+  }
+  if (account == null){
+    MessageBox.alert('请重新登录', '登录过期', {
+      confirmButtonText: '确定',
+      callback: action => {
+        next('/')
+      }
+    })
+  }else {
+    next();
+  }
 })
 
 export default router
